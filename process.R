09d101cc94f39a6ce9e214793fd54d974c60de53
Emily Ding <emilyd61@uw.edu>
2018-05-24 16:46:47 -0700

    modified the data set

--- process.R
+++ process.R
@@ -2,11 +2,6 @@
 ## SET UP ##
 ############
 
-<<<<<<< HEAD
-library("httr")
-library("jsonlite")
-=======
->>>>>>> dataset
 library("tidyr")
 library("ggplot2")
 library("dplyr")
@@ -15,15 +10,6 @@ library("plotly")
 library("readr")
 library("stringr")
 
-<<<<<<< HEAD
-source("api_key.R")
-base_uri <- "https://api.data.gov/ed/collegescorecard/v1/schools"
-parameter <- list("api_key" = apiKey)
-response <- GET(paste0(base_uri, ""), query = parameter)
-body <- fromJSON(content(response, "text"))
-df <- flatten(body$results)
+df <- read.csv("data/MERGED2006-2015.csv", stringsAsFactors = FALSE)
 View(df)
-=======
-df <- read.csv("data/MERGED2006_V1.csv", stringsAsFactors = FALSE)
-View(df)
->>>>>>> dataset
+
